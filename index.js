7332113bd7402d8aa87d2137fc8740cbfad3eb2c
Edith Gatwiri <edithgatwiri70@gmail.com>
2024-12-05 07:12:30 +0300

    array find

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(arr) {
+    const win= arr.find(arr => arr.result === "W")
+    return win ? win.year : undefined;
+}
+const arr = [
+    { year: "2018", result: "L" },
+    { year: "2019", result: "W" },
+    { year: "2020", result: "L" },
+];
+
+console.log(superbowlWin(arr)); 
+console.log(superbowlWin([])); 
